3824a9c79f2964fc799af7d9907b967d59f2f3d1
AlvaroRojas <alrojimenez@hotmail.com>
2023-04-30 10:50:15 +0200

    Metadada properties for windows

--- MediaManager/Platforms/Uap/Media/MediaItemExtensions.cs
+++ MediaManager/Platforms/Uap/Media/MediaItemExtensions.cs
@@ -3,6 +3,7 @@ using MediaManager.Media;
 using Windows.Media.Core;
 using Windows.Media.Playback;
 using Windows.Storage;
+using Windows.Storage.Streams;
 
 namespace MediaManager.Platforms.Uap.Media
 {
@@ -10,29 +11,64 @@ namespace MediaManager.Platforms.Uap.Media
     {
         public static async Task<MediaSource> ToMediaSource(this IMediaItem mediaItem)
         {
-            //TODO: Get Metadata from MediaSource
+            MediaSource res = null;
+            var PlayListMediaItem = mediaItem;
             if (mediaItem.MediaLocation.IsLocal())
             {
                 var storageFile = await StorageFile.GetFileFromPathAsync(mediaItem.MediaUri);
-                return MediaSource.CreateFromStorageFile(storageFile);
+                res = MediaSource.CreateFromStorageFile(storageFile);
             }
             else if (mediaItem.MediaLocation == MediaLocation.InMemory)
-                return MediaSource.CreateFromStream(mediaItem.Data.AsRandomAccessStream(), mediaItem.MimeType.ToMimeTypeString());
+            {
+                res = MediaSource.CreateFromStream(mediaItem.Data.AsRandomAccessStream(), mediaItem.MimeType.ToMimeTypeString());
+            }
             else
-                return MediaSource.CreateFromUri(new Uri(mediaItem.MediaUri));
+            {
+                res = MediaSource.CreateFromUri(new Uri(mediaItem.MediaUri));
+            }
+            if (PlayListMediaItem is not null)
+            {
+                res.CustomProperties["Id"] = PlayListMediaItem.Id;
+                res.CustomProperties["Album"] = PlayListMediaItem.Album;
+                res.CustomProperties["DisplayTitle"] = PlayListMediaItem.DisplayTitle;
+                res.CustomProperties["Artist"] = PlayListMediaItem.Artist;
+                res.CustomProperties["TrackNumber"] = PlayListMediaItem.TrackNumber;
+                res.CustomProperties["Thumbnail"] = PlayListMediaItem.AlbumImageUri;
+            }
+            return res;
         }
 
         public static MediaPlaybackItem ToMediaPlaybackItem(this MediaSource mediaSource)
         {
-            return new MediaPlaybackItem(mediaSource);
+            MediaPlaybackItem res;
+            res = new MediaPlaybackItem(mediaSource);
+            var displayProperties = res.GetDisplayProperties();
+            displayProperties.Type = Windows.Media.MediaPlaybackType.Music;
+            displayProperties.MusicProperties.AlbumTitle = (string)mediaSource.CustomProperties["Album"];
+            displayProperties.MusicProperties.Title = (string)mediaSource.CustomProperties["DisplayTitle"];
+            displayProperties.MusicProperties.Artist = (string)mediaSource.CustomProperties["Artist"];
+            displayProperties.MusicProperties.TrackNumber = uint.Parse(mediaSource.CustomProperties["TrackNumber"].ToString());
+            displayProperties.Thumbnail = RandomAccessStreamReference.CreateFromUri(new Uri((string)mediaSource.CustomProperties["Thumbnail"]));
+            res.ApplyDisplayProperties(displayProperties);
+            return res;
         }
 
         public static MediaPlaybackItem ToMediaPlaybackItem(this MediaSource mediaSource, TimeSpan startAt, TimeSpan? stopAt = null)
         {
+            MediaPlaybackItem res;
             if (stopAt is TimeSpan endTime)
-                return new MediaPlaybackItem(mediaSource, startAt, endTime);
+                res = new MediaPlaybackItem(mediaSource, startAt, endTime);
 
-            return new MediaPlaybackItem(mediaSource, startAt);
+            res = new MediaPlaybackItem(mediaSource, startAt);
+            var displayProperties = res.GetDisplayProperties();
+            displayProperties.MusicProperties.AlbumTitle = (string)mediaSource.CustomProperties["Album"];
+            displayProperties.MusicProperties.Title = (string)mediaSource.CustomProperties["DisplayTitle"];
+            displayProperties.MusicProperties.Artist = (string)mediaSource.CustomProperties["Artist"];
+            displayProperties.MusicProperties.TrackNumber = uint.Parse(mediaSource.CustomProperties["TrackNumber"].ToString());
+            displayProperties.Thumbnail = RandomAccessStreamReference.CreateFromUri(new Uri((string)mediaSource.CustomProperties["Thumbnail"]));
+            res.ApplyDisplayProperties(displayProperties);
+
+            return res;
         }
     }
 }
